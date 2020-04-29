d3fce0d0200d7b63456f54b0631ba5c33b22d412
Narek19971 <natabekyan.1997@gmail.com>
2020-04-29 14:16:10 +0400

    install ejs, body-parser

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.render('index')
 })
 
-app.listen(3000, () => console.log(`server is run in port 3000`))
+app.listen(3000, () => console.log(`server is run in port 3000`));
+
